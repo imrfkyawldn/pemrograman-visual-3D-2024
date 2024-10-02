5f27f75726c8e040f97809806b77670e91c7b825
naufaldi4413 <muhammadnaufaldi160@gmail.com>
2024-10-02 17:41:03 +0700

    230441100134_MuhammadRifkyAwaludin_CindyPermatasari_Modul1

---
+++ modul-1/230441100134_MuhammadRifkyAwaludin_Modul1/src/main/java/com/mycompany/formulirdatasiswa/FormulirDataSiswa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulirdatasiswa;
+
+/**
+ *
+ * @author Aldinn
+ */
+public class FormulirDataSiswa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
