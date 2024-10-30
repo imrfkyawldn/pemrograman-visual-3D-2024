fa4f49e86cdde6ffa2997bc5d58a234ba5e405e5
naufaldi4413 <muhammadnaufaldi160@gmail.com>
2024-10-30 19:28:13 +0700

    230441100134_MuhammadRifkyAwaludin_Modul4_KakCindyPermatasari

---
+++ modul-4/230441100134_MuhammadRifkyAwaludin_Modul4/src/main/java/com/mycompany/permainansederhana/PermainanSederhana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.permainansederhana;
+
+/**
+ *
+ * @author Aldinn
+ */
+public class PermainanSederhana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
